3e82698b56eab68e4e2f659495a659efa255d5ea
nathanim1919 <nathanim2tadele@gmail.com>
2025-09-14 01:18:19 +0300

    feat: Integrate React Query for data fetching; add QueryClientProvider and ReactQueryDevtools in main component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "sonner";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/query-client.ts";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {
+        import.meta.env.DEV && <ReactQueryDevtools />
+      }
+    </QueryClientProvider>
     <Toaster
       position="top-right"
       theme="dark"
